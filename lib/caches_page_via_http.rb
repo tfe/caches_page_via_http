f73d35c06663f92de26e006f9bea91fcbfdcf448
Todd Eichel <todd@toddeichel.com>
2010-09-30 03:31:46 -0400

    Add ability to specify custom TTL.

--- lib/caches_page_via_http.rb
+++ lib/caches_page_via_http.rb
@@ -12,10 +12,13 @@ module ActionController
 
         def caches_page(*actions)
           return unless perform_caching
+          
           options = actions.respond_to?(:extract_options!) ? actions.extract_options! : {}
+          ttl = options[:expires_in] || 300
+          
           after_filter({:only => actions}.merge(options)) do |c|
-            c.response.headers['Cache-Control'] = 'public, max-age=300'
-            c.response.headers['Expires'] = 300.seconds.from_now.httpdate
+            c.response.headers['Cache-Control'] = "public, max-age=#{ttl}"
+            c.response.headers['Expires'] = ttl.seconds.from_now.httpdate
             c.response.headers.delete('cookie') # caching the page means no cookies
           end
         end
